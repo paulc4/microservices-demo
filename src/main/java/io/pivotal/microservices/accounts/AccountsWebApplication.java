b667304b8363bb9489ac9fff4fcac2c691a537e0
Paul Chapman <chapman@zemsys.com>
2015-07-03 01:08:20 +0700

    Tidying

--- src/main/java/io/pivotal/microservices/accounts/AccountsWebApplication.java
+++ src/main/java/io/pivotal/microservices/accounts/AccountsWebApplication.java
@@ -1,5 +1,7 @@
 package io.pivotal.microservices.accounts;
 
+import io.pivotal.microservices.services.accounts.AccountsServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,7 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * Run the Accounts Service as a stand-alone Spring Boot web-application.
+ * Run the Accounts Service as a stand-alone Spring Boot web-application for
+ * testing. To run as a microservice, use {@link AccountsServer}.
  * 
  * @author Paul Chapman
  */
@@ -15,9 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @EnableJpaRepositories("io.pivotal.microservices.accounts")
 @ComponentScan("io.pivotal.microservices.accounts")
-public class AccountsWebServiceApplication {
+public class AccountsWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountsWebServiceApplication.class, args);
+		SpringApplication.run(AccountsWebApplication.class, args);
 	}
 }
