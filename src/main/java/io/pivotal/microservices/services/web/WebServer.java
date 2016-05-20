83892309aac1b43204987ad995cb12e89d1f61f0
Paul Chapman <chapman@zemsys.com>
2016-05-21 02:09:26 +1000

    Upgrade for Brixton release-train

--- src/main/java/io/pivotal/microservices/services/web/WebServer.java
+++ src/main/java/io/pivotal/microservices/services/web/WebServer.java
@@ -3,8 +3,10 @@ package io.pivotal.microservices.services.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Accounts web-server. Works as a microservice client, fetching data from the
@@ -33,6 +35,18 @@ public class WebServer {
 		// Tell server to look for web-server.properties or web-server.yml
 		System.setProperty("spring.config.name", "web-server");
 		SpringApplication.run(WebServer.class, args);
+	}
+
+	/**
+	 * A customized RestTemplate that has the ribbon load balancer build in.
+	 * Note that prior to the "Brixton" 
+	 * 
+	 * @return
+	 */
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 	/**
