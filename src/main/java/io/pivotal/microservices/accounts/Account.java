b876884f72c4550145c1a32f6f3fafce73361e3d
Paul Chapman <chapman@zemsys.com>
2016-05-21 00:22:08 +1000

    Remove unused import

--- src/main/java/io/pivotal/microservices/accounts/Account.java
+++ src/main/java/io/pivotal/microservices/accounts/Account.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
