7aeba3ffa14a7ef3e8ebebbbfedaa7ea40c61782
Paul Chapman <chapman@zemsys.com>
2018-06-24 00:21:19 +1000

    Spring Boot 2 - new actuator URLs/properties

--- src/main/java/io/pivotal/microservices/services/accounts/AccountsServer.java
+++ src/main/java/io/pivotal/microservices/services/accounts/AccountsServer.java
@@ -16,6 +16,9 @@ import io.pivotal.microservices.accounts.AccountsConfiguration;
  * <p>
  * Note that the configuration for this application is imported from
  * {@link AccountsConfiguration}. This is a deliberate separation of concerns.
+ * <p>
+ * This class declares no beans and current package contains no components for
+ * ComponentScan to find. No point using <tt>@SptingBootApplication</tt>.
  * 
  * @author Paul Chapman
  */
