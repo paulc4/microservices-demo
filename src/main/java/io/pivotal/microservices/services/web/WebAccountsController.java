6480388999d42bc972b8a250617667067c633bb9
Paul Chapman <pchapman@nuodb.com>
2021-01-16 22:59:54 +0000

    Update web templates - Move to Bootstrap 4 and later version of jQuery - Add error handling to WebAccountsController (account not found) - Revamp web-pages to use new styles - Replace Pivotal logo image

--- src/main/java/io/pivotal/microservices/services/web/WebAccountsController.java
+++ src/main/java/io/pivotal/microservices/services/web/WebAccountsController.java
@@ -1,4 +1,5 @@
 package io.pivotal.microservices.services.web;
+
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -22,72 +23,75 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WebAccountsController {
 
-	@Autowired
-	protected WebAccountsService accountsService;
+    @Autowired
+    protected WebAccountsService accountsService;
 
-	protected Logger logger = Logger.getLogger(WebAccountsController.class
-			.getName());
+    protected Logger logger = Logger.getLogger(WebAccountsController.class.getName());
 
-	public WebAccountsController(WebAccountsService accountsService) {
-		this.accountsService = accountsService;
-	}
+    public WebAccountsController(WebAccountsService accountsService) {
+        this.accountsService = accountsService;
+    }
 
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		binder.setAllowedFields("accountNumber", "searchText");
-	}
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.setAllowedFields("accountNumber", "searchText");
+    }
 
-	@RequestMapping("/accounts")
-	public String goHome() {
-		return "index";
-	}
+    @RequestMapping("/accounts")
+    public String goHome() {
+        return "index";
+    }
 
-	@RequestMapping("/accounts/{accountNumber}")
-	public String byNumber(Model model,
-			@PathVariable("accountNumber") String accountNumber) {
+    @RequestMapping("/accounts/{accountNumber}")
+    public String byNumber(Model model, @PathVariable("accountNumber") String accountNumber) {
 
-		logger.info("web-service byNumber() invoked: " + accountNumber);
+        logger.info("web-service byNumber() invoked: " + accountNumber);
 
-		Account account = accountsService.findByNumber(accountNumber);
-		logger.info("web-service byNumber() found: " + account);
-		model.addAttribute("account", account);
-		return "account";
-	}
+        Account account = accountsService.findByNumber(accountNumber);
 
-	@RequestMapping("/accounts/owner/{text}")
-	public String ownerSearch(Model model, @PathVariable("text") String name) {
-		logger.info("web-service byOwner() invoked: " + name);
+        if (account == null) { // no such account
+            model.addAttribute("number", accountNumber);
+            return "account";
+        }
 
-		List<Account> accounts = accountsService.byOwnerContains(name);
-		logger.info("web-service byOwner() found: " + accounts);
-		model.addAttribute("search", name);
-		if (accounts != null)
-			model.addAttribute("accounts", accounts);
-		return "accounts";
-	}
+        logger.info("web-service byNumber() found: " + account);
+        model.addAttribute("account", account);
+        return "account";
+    }
 
-	@RequestMapping(value = "/accounts/search", method = RequestMethod.GET)
-	public String searchForm(Model model) {
-		model.addAttribute("searchCriteria", new SearchCriteria());
-		return "accountSearch";
-	}
+    @RequestMapping("/accounts/owner/{text}")
+    public String ownerSearch(Model model, @PathVariable("text") String name) {
+        logger.info("web-service byOwner() invoked: " + name);
 
-	@RequestMapping(value = "/accounts/dosearch")
-	public String doSearch(Model model, SearchCriteria criteria,
-			BindingResult result) {
-		logger.info("web-service search() invoked: " + criteria);
+        List<Account> accounts = accountsService.byOwnerContains(name);
+        logger.info("web-service byOwner() found: " + accounts);
+        model.addAttribute("search", name);
+        if (accounts != null)
+            model.addAttribute("accounts", accounts);
+        return "accounts";
+    }
 
-		criteria.validate(result);
+    @RequestMapping(value = "/accounts/search", method = RequestMethod.GET)
+    public String searchForm(Model model) {
+        model.addAttribute("searchCriteria", new SearchCriteria());
+        return "accountSearch";
+    }
 
-		if (result.hasErrors())
-			return "accountSearch";
+    @RequestMapping(value = "/accounts/dosearch")
+    public String doSearch(Model model, SearchCriteria criteria, BindingResult result) {
+        logger.info("web-service search() invoked: " + criteria);
 
-		String accountNumber = criteria.getAccountNumber();
-		if (StringUtils.hasText(accountNumber)) {
-			return byNumber(model, accountNumber);
-		} else {
-			String searchText = criteria.getSearchText();
-			return ownerSearch(model, searchText);
-		}
-	}
+        criteria.validate(result);
+
+        if (result.hasErrors())
+            return "accountSearch";
+
+        String accountNumber = criteria.getAccountNumber();
+        if (StringUtils.hasText(accountNumber)) {
+            return byNumber(model, accountNumber);
+        } else {
+            String searchText = criteria.getSearchText();
+            return ownerSearch(model, searchText);
+        }
+    }
 }
--- src/main/java/io/pivotal/microservices/services/web/WebAccountsService.java
+++ src/main/java/io/pivotal/microservices/services/web/WebAccountsService.java
@@ -22,64 +22,63 @@ import io.pivotal.microservices.exceptions.AccountNotFoundException;
 @Service
 public class WebAccountsService {
 
-	@Autowired
-	@LoadBalanced
-	protected RestTemplate restTemplate;
+    @Autowired
+    @LoadBalanced
+    protected RestTemplate restTemplate;
 
-	protected String serviceUrl;
+    protected String serviceUrl;
 
-	protected Logger logger = Logger.getLogger(WebAccountsService.class
-			.getName());
+    protected Logger logger = Logger.getLogger(WebAccountsService.class.getName());
 
-	public WebAccountsService(String serviceUrl) {
-		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl
-				: "http://" + serviceUrl;
-	}
+    public WebAccountsService(String serviceUrl) {
+        this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl : "http://" + serviceUrl;
+    }
 
-	/**
-	 * The RestTemplate works because it uses a custom request-factory that uses
-	 * Ribbon to look-up the service to use. This method simply exists to show
-	 * this.
-	 */
-	@PostConstruct
-	public void demoOnly() {
-		// Can't do this in the constructor because the RestTemplate injection
-		// happens afterwards.
-		logger.warning("The RestTemplate request factory is "
-				+ restTemplate.getRequestFactory().getClass());
-	}
+    /**
+     * The RestTemplate works because it uses a custom request-factory that uses
+     * Ribbon to look-up the service to use. This method simply exists to show this.
+     */
+    @PostConstruct
+    public void demoOnly() {
+        // Can't do this in the constructor because the RestTemplate injection
+        // happens afterwards.
+        logger.warning("The RestTemplate request factory is " + restTemplate.getRequestFactory().getClass());
+    }
 
-	public Account findByNumber(String accountNumber) {
+    public Account findByNumber(String accountNumber) {
 
-		logger.info("findByNumber() invoked: for " + accountNumber);
-		return restTemplate.getForObject(serviceUrl + "/accounts/{number}",
-				Account.class, accountNumber);
-	}
+        logger.info("findByNumber() invoked: for " + accountNumber);
+        try {
+            return restTemplate.getForObject(serviceUrl + "/accounts/{number}", Account.class, accountNumber);
+        } catch (Exception e) {
+            logger.severe(e.getClass() + ": " + e.getLocalizedMessage());
+            return null;
+        }
 
-	public List<Account> byOwnerContains(String name) {
-		logger.info("byOwnerContains() invoked:  for " + name);
-		Account[] accounts = null;
+    }
 
-		try {
-			accounts = restTemplate.getForObject(serviceUrl
-					+ "/accounts/owner/{name}", Account[].class, name);
-		} catch (HttpClientErrorException e) { // 404
-			// Nothing found
-		}
+    public List<Account> byOwnerContains(String name) {
+        logger.info("byOwnerContains() invoked:  for " + name);
+        Account[] accounts = null;
 
-		if (accounts == null || accounts.length == 0)
-			return null;
-		else
-			return Arrays.asList(accounts);
-	}
+        try {
+            accounts = restTemplate.getForObject(serviceUrl + "/accounts/owner/{name}", Account[].class, name);
+        } catch (HttpClientErrorException e) { // 404
+            // Nothing found
+        }
 
-	public Account getByNumber(String accountNumber) {
-		Account account = restTemplate.getForObject(serviceUrl
-				+ "/accounts/{number}", Account.class, accountNumber);
+        if (accounts == null || accounts.length == 0)
+            return null;
+        else
+            return Arrays.asList(accounts);
+    }
 
-		if (account == null)
-			throw new AccountNotFoundException(accountNumber);
-		else
-			return account;
-	}
+    public Account getByNumber(String accountNumber) {
+        Account account = restTemplate.getForObject(serviceUrl + "/accounts/{number}", Account.class, accountNumber);
+
+        if (account == null)
+            throw new AccountNotFoundException(accountNumber);
+        else
+            return account;
+    }
 }
